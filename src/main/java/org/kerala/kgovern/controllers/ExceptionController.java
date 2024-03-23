8029fd3acd21a0503b7a72dd702b7ae2952f2d90
Shamal Muneer <shamal20017@gmail.com>
2024-03-23 20:50:31 +0530

    prolly final commit

--- src/main/java/org/kerala/kgovern/controllers/ExceptionController.java
+++ src/main/java/org/kerala/kgovern/controllers/ExceptionController.java
@@ -1,6 +1,8 @@
 package org.kerala.kgovern.controllers;
 
 import org.kerala.kgovern.exceptions.InternalServerError;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,10 +10,12 @@ import org.springframework.web.client.HttpClientErrorException.NotFound;
 
 @ControllerAdvice
 public class ExceptionController {
-    @ExceptionHandler(InternalServerError.class)
+    @ExceptionHandler(Exception.class)
+    @Order(Ordered.LOWEST_PRECEDENCE)
     public String internalServerError(){
         return "500";
     }
+    
     @ExceptionHandler(NotFound.class)
     public String notFoundError(){
         return "400";
